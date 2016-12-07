dd37cacd623b0b085a7965abe0973b410e3380e2
JacobBrunsting <jacob.brunsting@gmail.com>
2016-12-06 23:21:33 -0500

    Things work

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ angular.module('openwindow', []).controller('windowopener', [
         function($scope) {
             $scope.test = "this is a test";
             $scope.post_prefix = ":::";
-            $scope.posts = ["post 1", "post 2", "post 3"];
-            $scope.posts_suffix = ";;;";
+            $scope.posts = [
+            {title:'Title1', body:'Body 1'},
+            {title:'Title2', body:'Bdy 1'},
+            {title:'Title3', body:'By 1'},
+            {title:'Title4', body:'o 1'}
+            ];
+            $scope.post_suffix = ";;;";
         }
 ]);
