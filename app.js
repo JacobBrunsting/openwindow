496967496ba6f3d67a0cf0cb3db4421a59fa6751
JacobBrunsting <jacob.brunsting@gmail.com>
2016-12-06 23:58:33 -0500

    Used tutorial to hack in buttons

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 angular.module('openwindow', []).controller('windowopener', [
         '$scope',
         function($scope) {
+            // Title
             $scope.test = "this is a test";
+            // Posts
             $scope.post_prefix = ":::";
             $scope.posts = [
             {title:'Title1', body:'Body 1'},
@@ -10,5 +12,9 @@ angular.module('openwindow', []).controller('windowopener', [
             {title:'Title4', body:'o 1'}
             ];
             $scope.post_suffix = ";;;";
+            // Input
+            $scope.addPost = function() {
+                $scope.posts.push({title:'new',body:'other'});
+            }
         }
 ]);
