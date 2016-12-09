82846f939af567be23d13a918f205bc70c6e73ba
JacobBrunsting <jacob.brunsting@gmail.com>
2016-12-08 23:42:06 -0500

    Made different pages that redirect to eachother

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-angular.module('openwindow', []).controller('windowopener', [
+app = angular.module('openwindow', ['ui.router']);
+
+app.controller('windowopener', [
         '$scope',
         function($scope) {
             // Title
@@ -16,5 +18,42 @@ angular.module('openwindow', []).controller('windowopener', [
             $scope.addPost = function() {
                 $scope.posts.push({title:'new',body:'other'});
             }
+            $scope.addCustomPost = function() {
+                if ($scope.title != '') {
+                    $scope.posts.push({title: $scope.title,body:'bod'});
+                    $scope.title = '';
+                }
+            }
         }
+]);
+
+app.controller('windowcreator', [
+        '$scope',
+        '$window',
+        function($scope, $window) {
+            $scope.test = "You should be on the new message page now";
+            $scope.addCustomPost = function() {
+                // add a post to the database
+                $scope.test = "changed";
+                $window.location.href = '#/home';
+            }
+        }
+]);
+
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: '/home',
+            templateUrl: '/home.html',
+            controller: 'windowopener'
+        });
+        $stateProvider.state('new', {
+            url: '/new',
+            templateUrl: '/newpost.html',
+            controller: 'windowcreator'
+        });
+        $urlRouterProvider.otherwise('home');
+    }
 ]);
