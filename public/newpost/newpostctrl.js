1f0ba89cf32f66001a62d82e38da4846598fdf91
JacobBrunsting <jacob.brunsting@gmail.com>
2016-12-10 00:39:54 -0500

    Made backend recieve api requests

--- public/newpost/newpostctrl.js
+++ public/newpost/newpostctrl.js
@@ -1,12 +1,16 @@
 angular.module('openwindow').controller('newpostctrl', [
         '$scope',
         '$window',
-        function($scope, $window) {
+        '$http',
+        function($scope, $window, $http) {
             $scope.test = "You should be on the new message page now";
-            $scope.addCustomPost = function() {
+            $scope.createNewSitePost = function() {
                 // add a post to the database
                 $scope.test = "changed";
                 $window.location.href = '#/home';
+                var sitepost = {title : $scope.title, body : 'TEST'};
+                $scope.title = '';
+                $http.post("/api/sitepost", sitepost);
             }
         }
 ]);
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.json()); // lots of other parsers you can use!
 app.use(express.static(__dirname + '/public'));
+
+app.post("/api/sitepost", addNewSitePost);
+
+function addNewSitePost(request, response) {
+    var sitePost = request.body;
+    console.log("added new post " + sitePost.title + ", " + sitePost.body);
+}
 
 app.listen(3000);
 
