65112110a8ddf88b12ec51d28acf63aa68c590ab
JacobBrunsting <jacob.brunsting@gmail.com>
2016-12-16 00:48:00 -0500

    Added some styling

--- public/home/homectrl.js
+++ public/home/homectrl.js
@@ -5,14 +5,12 @@ angular.module('openwindow').controller('homectrl', [
             // Title
             $scope.test = "this is a test";
             // Posts
-            $scope.post_prefix = ":::";
             $scope.posts = [
             {title:'Title1', body:'Body 1'},
             {title:'Title2', body:'Bdy 1'},
             {title:'Title3', body:'By 1'},
             {title:'Title4', body:'o 1'}
             ];
-            $scope.post_suffix = ";;;";
             // Input
             $scope.addPost = function() {
                 $scope.posts.push({title:'new',body:'other'});
