5848e91446e4558f42cb42c2bccebf39ce91f657
JacobBrunsting <jacob.brunsting@gmail.com>
2017-01-15 09:46:37 -0500

    Added fake location data to allow for testing until https certified

--- public/services/geolocation.js
+++ public/services/geolocation.js
@@ -1,6 +1,9 @@
 // This is a service to allow for changing location retriever later
 angular.module('openwindow').service('geolocation', [function() {
     this.get = function(onSuccess, onError) {
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        // uncomment once https certified (getCurrentPosition only works with
+        // https)
+        // navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        onSuccess({longitude:0, latitude:0});
     }
 }]);
