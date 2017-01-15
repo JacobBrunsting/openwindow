51430eacd84ab2aed7ec361448d144f8834c6879
JacobBrunsting <jacob.brunsting@gmail.com>
2017-01-14 22:07:02 -0500

    Added post_updater service file previously missing from git

---
+++ public/services/post_updater.js
@@ -0,0 +1,12 @@
+angular.module('openwindow').service('post_updater', ['$interval', '$http', function($interval, $http) {
+    this.startUpdatingPosts = function(posts, updateInterval, callback) {
+        $interval(function(response) {
+            $http.get("/api/poststimeleft").success(function(response) {
+                for (var i = 0; i < posts.length; ++i) {
+                    var original = posts[i].secondsToShowFor;
+                    posts[i].secondsToShowFor = response[posts[i].id];
+                }
+            })
+        }, updateInterval);
+    }
+}]);
