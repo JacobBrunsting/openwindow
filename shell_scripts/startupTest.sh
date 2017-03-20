c83654a6eb55cdfb89d02648c9cb7d79cbc2fcd3
JacobBrunsting <jacob.brunsting@gmail.com>
2017-03-20 00:10:07 -0400

    Make startup script run another database server for testing

--- shell_scripts/startupTest.sh
+++ shell_scripts/startupTest.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 gnome-terminal --window-with-profile=solarized-dark-small -x sh -c '
-node --use-strict ../webapp/webapp.js firstSetup=true databaseServersInfoCollection=DatabaseServersInfo1 webServersInfoCollection=WebServersInfoCollection1 port=5000;
-echo "\n\nEND OF EXECUTION";
-sleep infinity;
+node --use-strict ../webapp/webapp.js firstSetup=true databaseServersInfoCollection=DatabaseServersInfo1 webServersInfoCollection=WebServersInfoCollection1 port=8080
+echo "\n\nEND OF EXECUTION"
+sleep infinity
 '
-sleep 2;
+sleep 2
 gnome-terminal --window-with-profile=solarized-dark-small -x sh -c '
-node --use-strict ../post_database/post_database.js postModelName=Posts1 backupPostModelName=BackupPosts1 port=6000;
-echo "\n\nEND OF EXECUTION";
-sleep infinity;
+node --use-strict ../post_database/post_database.js postModelName=Posts1 backupPostModelName=BackupPosts1 port=6000
+echo "\n\nEND OF EXECUTION"
+sleep infinity
 '
-sleep 2;
+sleep 2
 gnome-terminal --window-with-profile=solarized-dark-small -x sh -c '
-node --use-strict ../webapp/webapp.js databaseServersInfoCollection=DatabaseServersInfo1 webServersInfoCollection=WebServersInfoCollection1 port=7000;
-echo "\n\nEND OF EXECUTION";
-sleep infinity;
+node --use-strict ../webapp/webapp.js databaseServersInfoCollection=DatabaseServersInfo2 webServersInfoCollection=WebServersInfoCollection2 port=7000
+echo "\n\nEND OF EXECUTION"
+sleep infinity
+'
+sleep 2
+gnome-terminal --window-with-profile=solarized-dark-small -x sh -c '
+node --use-strict ../post_database/post_database.js postModelName=Posts2 backupPostModelName=BackupPosts2 port=8000
+echo "\n\nEND OF EXECUTION"
+sleep infinity
 '
