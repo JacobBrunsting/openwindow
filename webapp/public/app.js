13fa6822a5e00181d1cd461b0dab5de9c59d284a
JacobBrunsting <jacob.brunsting@gmail.com>
2017-03-28 23:18:49 -0400

    Increase post retrieval radius and add ability to add custom locations to aid testing

--- webapp/public/app.js
+++ webapp/public/app.js
@@ -1,6 +1,6 @@
 angular.module('openwindow', ['ui.router'])
     .constant('INT_CONSTANTS', {
-        'POST_RADIUS': 50000,
+        'POST_RADIUS': 500000,
         'UPVOTE': 2,
         'DOWNVOTE': 1,
         'NONE': 0
--- webapp/public/home/homectrl.js
+++ webapp/public/home/homectrl.js
@@ -18,7 +18,8 @@ angular.module('openwindow').controller('homectrl', [
         function onLocationRetrievalFailure(err) {
             console.log(err);
         }
-        geolocation.get(setupPage, onLocationRetrievalFailure);
+        //geolocation.get(setupPage, onLocationRetrievalFailure);
+        setupPage(getTestingLocation());
 
         function getAllPosts() {
             $scope.page = "home";
@@ -36,6 +37,19 @@ angular.module('openwindow').controller('homectrl', [
                     var UPDATE_INTERVAL = 10000;
                     post_updater.startUpdatingPosts($scope.posts, $scope.location, UPDATE_INTERVAL);
                 });
+        }
+
+        function getTestingLocation() {
+            let longitude;
+            let latitude;
+            do {
+                if (longitude && latitude) {
+                    console.log("The current coordinates are invalid, please try again");
+                }
+                longitude = prompt("Testing only: Enter the testing user's longitude (-180 - 180)");
+                latitude = prompt("Testing only: Enter the testing user's latitude (-90 - 90)")
+            } while(isNaN(longitude) || isNaN(latitude) || longitude < -180 || longitude > 180 || latitude < -90 || latitude > 90);
+            return {longitude: longitude, latitude: latitude};
         }
     }
 ]);
