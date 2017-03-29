14b54c87b7ef0d359329b7be88b658e7c75553ab
JacobBrunsting <jacob.brunsting@gmail.com>
2017-03-28 22:41:13 -0400

    Change testing script to remove an issue where two server instances were using the same post database

--- shell_scripts/startupTest.sh
+++ shell_scripts/startupTest.sh
@@ -24,7 +24,7 @@ sleep infinity
 '
 sleep 2
 gnome-terminal --window-with-profile=solarized-dark-small -x sh -c '
-node --use-strict ../post_database/post_database.js postModelName=Posts2 backupPostModelName=BackupPosts2 port=5000
+node --use-strict ../post_database/post_database.js postModelName=Posts3 backupPostModelName=BackupPosts3 port=5000
 echo "\n\nEND OF EXECUTION"
 sleep infinity
 '
