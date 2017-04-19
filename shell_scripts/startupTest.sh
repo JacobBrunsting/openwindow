89c742fa29c7956f3b3786b19d63bf7696bb7468
JacobBrunsting <jacob.brunsting@gmail.com>
2017-04-18 23:09:43 -0400

    Add 'press enter to exit' prompt to server terminal windows started by the startup script that is shown once a server crashes or is killed'

--- shell_scripts/startupTest.sh
+++ shell_scripts/startupTest.sh
@@ -10,48 +10,48 @@ kill -9 $(lsof -t -i:3000 -sTCP:LISTEN)
 gnome-terminal --window-with-profile=solarized-dark-small -x sh -c '
 echo -ne "\033]0;port 8080 webserver\007"
 node --use-strict ../webapp/webapp.js firstSetup=true databaseServersInfoCollection=DatabaseServersInfo1 webServersInfoCollection=WebServersInfoCollection1 port=8080
-echo "\n\nEND OF EXECUTION"
-sleep 30
+echo "\n\nEND OF EXECUTION\n\nPress enter to exit"
+read _
 '
 sleep 1
 gnome-terminal --window-with-profile=solarized-dark-small -x sh -c '
 echo -ne "\033]0;port 6050 webserver\007"
 node --use-strict ../webapp/webapp.js databaseServersInfoCollection=DatabaseServersInfo2 webServersInfoCollection=WebServersInfoCollection2 port=6050
-echo "\n\nEND OF EXECUTION"
-sleep 30
+echo "\n\nEND OF EXECUTION\n\nPress enter to exit"
+read _
 '
 sleep 1
 gnome-terminal --window-with-profile=solarized-dark-small -x sh -c '
 echo -ne "\033]0;port 8008 database\007"
 node --use-strict ../post_database/post_database.js postModelName=Posts1 backupPostModelName=BackupPosts1 port=8008
-echo "\n\nEND OF EXECUTION"
-sleep 30
+echo "\n\nEND OF EXECUTION\n\nPress enter to exit"
+read _
 '
 sleep 1
 gnome-terminal --window-with-profile=solarized-dark-small -x sh -c '
 echo -ne "\033]0;port 3000 webserver\007"
 node --use-strict ../webapp/webapp.js databaseServersInfoCollection=DatabaseServersInfo3 webServersInfoCollection=WebServersInfoCollection3 port=3000
-echo "\n\nEND OF EXECUTION"
-sleep 30
+echo "\n\nEND OF EXECUTION\n\nPress enter to exit"
+read _
 '
 sleep 1
 gnome-terminal --window-with-profile=solarized-dark-small -x sh -c '
 echo -ne "\033]0;port 8000 database\007"
 node --use-strict ../post_database/post_database.js postModelName=Posts2 backupPostModelName=BackupPosts2 port=8000
-echo "\n\nEND OF EXECUTION"
-sleep 30
+echo "\n\nEND OF EXECUTION\n\nPress enter to exit"
+read _
 '
 sleep 1
 gnome-terminal --window-with-profile=solarized-dark-small -x sh -c '
 echo -ne "\033]0;port 5000 database\007"
 node --use-strict ../post_database/post_database.js postModelName=Posts3 backupPostModelName=BackupPosts3 port=5000
-echo "\n\nEND OF EXECUTION"
-sleep 30
+echo "\n\nEND OF EXECUTION\n\nPress enter to exit"
+read _
 '
 sleep 1
 gnome-terminal --window-with-profile=solarized-dark-small -x sh -c '
 echo -ne "\033]0;port 4000 database\007"
 node --use-strict ../post_database/post_database.js postModelName=Posts4 backupPostModelName=BackupPosts4 port=4000
-echo "\n\nEND OF EXECUTION"
-sleep 30
+echo "\n\nEND OF EXECUTION\n\nPress enter to exit"
+read _
 '
