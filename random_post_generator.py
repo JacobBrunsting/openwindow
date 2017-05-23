d0064f3551cddbf342cdd5750a79adcf0fa1bf66
JacobBrunsting <jacob.brunsting@gmail.com>
2017-05-23 19:44:18 -0400

    Make random post generator to pull quotes from the internet and create posts with them

---
+++ random_post_generator.py
@@ -0,0 +1,19 @@
+import requests
+import json
+import random
+import time
+from unidecode import unidecode
+
+SECONDS_BETWEEN_POSTS = 30;
+
+while True:
+    r = requests.get('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand')
+    quote = json.loads(r.content)[0]
+    title = unidecode(quote['title'] + ' (' + quote['link'] + ')')
+    body = unidecode(quote['content'].replace('<p>', '').replace('</p>', '').replace('</ br>', '')).replace('&#8220;', "'").replace('&#8221;', "'").replace('&#8211;', ',').replace('&#8217;', "'").replace('<strong>', '').replace('</strong>', '').replace('&#8216;', "'");
+    lat = random.sample(range(-90, 90), 1)[0]
+    lng = random.sample(range(-180, 180), 1)[0]
+    print 'sending: title: ' + title + ', body:' + body
+    r = requests.post('http://192.168.2.12:8080/api/post?latitude=' + str(lat) + '&longitude=' + str(lng) + '&radius=0', json={'title': title, 'body': body, 'secondsToShowFor': 16000, 'loc': {'coordinates': [lng, lat]}})
+    print r
+    time.sleep(SECONDS_BETWEEN_POSTS)
