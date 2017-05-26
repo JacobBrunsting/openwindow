808f48cc1f6dc0e1726a79beae26ea03083b8d63
JacobBrunsting <jacob.brunsting@gmail.com>
2017-05-25 21:41:18 -0500

    Improved random post generation

--- random_post_generator.py
+++ random_post_generator.py
@@ -4,13 +4,13 @@ import random
 import time
 from unidecode import unidecode
 
-SECONDS_BETWEEN_POSTS = 30;
+SECONDS_BETWEEN_POSTS = 20;
 
 while True:
     r = requests.get('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand')
     quote = json.loads(r.content)[0]
     title = unidecode(quote['title'] + ' (' + quote['link'] + ')')
-    body = unidecode(quote['content'].replace('<p>', '').replace('</p>', '').replace('</ br>', '')).replace('&#8220;', "'").replace('&#8221;', "'").replace('&#8211;', ',').replace('&#8217;', "'").replace('<strong>', '').replace('</strong>', '').replace('&#8216;', "'");
+    body = unidecode(quote['content'].replace('<p>', '').replace('</p>', '').replace('<br />', '')).replace('&#8220;', "'").replace('&#8221;', "'").replace('&#8211;', ',').replace('&#8217;', "'").replace('<strong>', '').replace('</strong>', '').replace('&#8216;', "'").replace('&#8220;', '...');
     lat = random.sample(range(-90, 90), 1)[0]
     lng = random.sample(range(-180, 180), 1)[0]
     print 'sending: title: ' + title + ', body:' + body
