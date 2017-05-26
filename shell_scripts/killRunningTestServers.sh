42cc264846321be4b19c98b5ba9104ad2be063c8
JacobBrunsting <jacob.brunsting@gmail.com>
2017-05-26 10:30:06 -0400

    Make the testing script ports contain the collection number

--- shell_scripts/killRunningTestServers.sh
+++ shell_scripts/killRunningTestServers.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+kill -9 $(lsof -t -i:8001 -sTCP:LISTEN)
+kill -9 $(lsof -t -i:8002 -sTCP:LISTEN)
+kill -9 $(lsof -t -i:8003 -sTCP:LISTEN)
+kill -9 $(lsof -t -i:9001 -sTCP:LISTEN)
+kill -9 $(lsof -t -i:9002 -sTCP:LISTEN)
+kill -9 $(lsof -t -i:9003 -sTCP:LISTEN)
+kill -9 $(lsof -t -i:9004 -sTCP:LISTEN)
 kill -9 $(lsof -t -i:8080 -sTCP:LISTEN)
-kill -9 $(lsof -t -i:8000 -sTCP:LISTEN)
-kill -9 $(lsof -t -i:8008 -sTCP:LISTEN)
-kill -9 $(lsof -t -i:9000 -sTCP:LISTEN)
-kill -9 $(lsof -t -i:6050 -sTCP:LISTEN)
-kill -9 $(lsof -t -i:5000 -sTCP:LISTEN)
-kill -9 $(lsof -t -i:4000 -sTCP:LISTEN)
-kill -9 $(lsof -t -i:3000 -sTCP:LISTEN)
 sleep 0.1
 while [[ $(wmctrl -l | grep port) ]]; do
     wmctrl -a port
--- shell_scripts/startupTest.sh
+++ shell_scripts/startupTest.sh
@@ -17,50 +17,50 @@ read _
 sleep 1
 
 gnome-terminal -x sh -c '
-echo -ne "\033]0;port 9000 webserver\007"
-node --use-strict ../webapp/webapp.js firstSetup=true databaseServersInfoCollection=DatabaseServersInfo1 webServersInfoCollection=WebServersInfoCollection1 port=9000
+echo -ne "\033]0;port 8001 webserver\007"
+node --use-strict ../webapp/webapp.js firstSetup=true databaseServersInfoCollection=DatabaseServersInfo1 webServersInfoCollection=WebServersInfoCollection1 port=8001
 echo "\n\nEND OF EXECUTION\n\nPress enter to exit"
 read _
 '
 sleep 1
 gnome-terminal -x sh -c '
-echo -ne "\033]0;port 6050 webserver\007"
-node --use-strict ../webapp/webapp.js databaseServersInfoCollection=DatabaseServersInfo2 webServersInfoCollection=WebServersInfoCollection2 port=6050
+echo -ne "\033]0;port 8002 webserver\007"
+node --use-strict ../webapp/webapp.js databaseServersInfoCollection=DatabaseServersInfo2 webServersInfoCollection=WebServersInfoCollection2 port=8002
 echo "\n\nEND OF EXECUTION\n\nPress enter to exit"
 read _
 '
 sleep 1
 gnome-terminal -x sh -c '
-echo -ne "\033]0;port 8008 database\007"
-node --use-strict ../post_database/post_database.js postModelName=Posts1 backupPostModelName=BackupPosts1 port=8008
+echo -ne "\033]0;port 9001 database\007"
+node --use-strict ../post_database/post_database.js postModelName=Posts1 backupPostModelName=BackupPosts1 port=9001
 echo "\n\nEND OF EXECUTION\n\nPress enter to exit"
 read _
 '
 sleep 1
 gnome-terminal -x sh -c '
-echo -ne "\033]0;port 3000 webserver\007"
-node --use-strict ../webapp/webapp.js databaseServersInfoCollection=DatabaseServersInfo3 webServersInfoCollection=WebServersInfoCollection3 port=3000
+echo -ne "\033]0;port 8003 webserver\007"
+node --use-strict ../webapp/webapp.js databaseServersInfoCollection=DatabaseServersInfo3 webServersInfoCollection=WebServersInfoCollection3 port=8003
 echo "\n\nEND OF EXECUTION\n\nPress enter to exit"
 read _
 '
 sleep 1
 gnome-terminal -x sh -c '
-echo -ne "\033]0;port 8000 database\007"
-node --use-strict ../post_database/post_database.js postModelName=Posts2 backupPostModelName=BackupPosts2 port=8000
+echo -ne "\033]0;port 9002 database\007"
+node --use-strict ../post_database/post_database.js postModelName=Posts2 backupPostModelName=BackupPosts2 port=9002
 echo "\n\nEND OF EXECUTION\n\nPress enter to exit"
 read _
 '
 sleep 1
 gnome-terminal -x sh -c '
-echo -ne "\033]0;port 5000 database\007"
-node --use-strict ../post_database/post_database.js postModelName=Posts3 backupPostModelName=BackupPosts3 port=5000
+echo -ne "\033]0;port 9003 database\007"
+node --use-strict ../post_database/post_database.js postModelName=Posts3 backupPostModelName=BackupPosts3 port=9003
 echo "\n\nEND OF EXECUTION\n\nPress enter to exit"
 read _
 '
 sleep 1
 gnome-terminal -x sh -c '
-echo -ne "\033]0;port 4000 database\007"
-node --use-strict ../post_database/post_database.js postModelName=Posts4 backupPostModelName=BackupPosts4 port=4000
+echo -ne "\033]0;port 9004 database\007"
+node --use-strict ../post_database/post_database.js postModelName=Posts4 backupPostModelName=BackupPosts4 port=9004
 echo "\n\nEND OF EXECUTION\n\nPress enter to exit"
 read _
 '
